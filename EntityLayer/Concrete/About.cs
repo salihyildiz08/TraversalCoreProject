0a7f37540cb1b10b7c384d9a5e7d406d16caf651
Salih YILDIZ <yldzsalih27@gmail.com>
2023-07-10 08:48:24 +0300

    Create Layers and Create Concrete folder in Entity Layer

---
+++ EntityLayer/Concrete/About.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+	public class About
+	{
+        public int ID { get; set; }
+        public string Details { get; set; }
+        public string Image { get; set; }
+        public bool status { get; set; }
+    }
+}

13094fe97a45d95388637996504603016a40730b
Salih YILDIZ <yldzsalih27@gmail.com>
2023-07-10 09:39:19 +0300

    Context Sınıfı oluşturuldu. Class'lara [Key] eklenildi.

--- EntityLayer/Concrete/About.cs
+++ EntityLayer/Concrete/About.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,13 @@ namespace EntityLayer.Concrete
 {
 	public class About
 	{
-        public int ID { get; set; }
+		[Key]
+		public int ID { get; set; }
         public string Title { get; set; }
         public string Title2 { get; set; }
         public string Description { get; set; }
         public string Description2 { get; set; }
-        public string Image1 { get; set; }
+        public string Image { get; set; }
         public bool status { get; set; }
     }
 }
--- EntityLayer/Concrete/Destination.cs
+++ EntityLayer/Concrete/Destination.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,8 @@ namespace EntityLayer.Concrete
 {
 	public class Destination
 	{
-        public int ID { get; set; }
+		[Key]
+		public int ID { get; set; }
         public string City { get; set; }
         public string DayNight { get; set; }
         public double Price { get; set; }

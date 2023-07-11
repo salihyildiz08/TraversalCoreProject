50547fb38db3237f0cb6864b1cce7b692832d652
Salih YILDIZ <yldzsalih27@gmail.com>
2023-07-11 09:32:04 +0300

    SubAbout componenti oluşturuldu. veriler çekildi.

---
+++ TraversalCoreProject/ViewComponents/Default/_SubAbout.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.Default
+{
+    public class _SubAbout:ViewComponent
+    {
+        SubAboutManager aboutManager=new SubAboutManager(new EfSubAboutDal(new Context()));
+        public IViewComponentResult Invoke()
+        {
+            var values = aboutManager.TGetList();
+            return View(values);
+        }
+    }
+}

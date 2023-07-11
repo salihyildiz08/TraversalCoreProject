8e3d5fcf61d7f98134d2b21a4c5ee9b16547f3fa
Salih YILDIZ <yldzsalih27@gmail.com>
2023-07-11 09:20:55 +0300

    Feature componenti yapıldı veriler listelendi.

---
+++ TraversalCoreProject/ViewComponents/Default/_Feature.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.Default
+{
+    public class _Feature:ViewComponent
+    {
+        FeatureManager featureManager = new FeatureManager(new EfFeatureDal(new Context()));
+
+        public IViewComponentResult Invoke()
+        {
+            var values = featureManager.TGetList();
+
+            return View(values);
+        }
+    }
+}

163a7ae0de997bf9e59b5800c36e2311666c6a5c
Salih YILDIZ <yldzsalih27@gmail.com>
2023-07-11 14:17:12 +0300

    Tur Rotaları sayfaları ve Detay sayfası yapıldı.

---
+++ TraversalCoreProject/Controllers/DestinationController.cs
@@ -0,0 +1,30 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Controllers
+{
+    public class DestinationController : Controller
+    {
+        DestinationManager _destinationManager = new DestinationManager(new EfDestinationDal(new Context()));
+        public IActionResult Index()
+        {
+            var values = _destinationManager.TGetList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult  Details(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Details(Destination p)
+        {
+            return View();
+        }
+    }
+}

ff5a87103dce277ff50d002fce83ba6681b16c8c
Salih YILDIZ <yldzsalih27@gmail.com>
2023-07-11 09:58:53 +0300

    Testimonial componenti yapıldı.

---
+++ TraversalCoreProject/ViewComponents/Default/_Testimonial.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.Default
+{
+    public class _Testimonial:ViewComponent
+    {
+        TestimonialManager testimonialManager=new TestimonialManager(new EfTestimonialDal(new Context()));
+        public IViewComponentResult Invoke()
+        {
+            var values = testimonialManager.TGetList();
+            return View(values);
+        }
+    }
+}

197928d33d45fff71594ef4dfb9f73bcd938bd19
Salih YILDIZ <yldzsalih27@gmail.com>
2023-08-07 14:20:33 +0300

    identiy register düzenlemeleri

---
+++ TraversalCoreProject/Models/UserRegisterViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TraversalCoreProject.Models
+{
+    public class UserRegisterViewModel
+    {
+        [Required(ErrorMessage ="Please Enter Your Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Your SurName")]
+        public string SurName { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Your UserName")]
+        public string UserName { get; set; }
+
+
+        [Required(ErrorMessage = "Please Enter Your Mail")]
+        public string Mail { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Your Password")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Your ConfirmPassword")]
+        [Compare("Password",ErrorMessage = "passwords are not compatible")]
+        public string ConfirmPassword { get; set; }
+    }
+}

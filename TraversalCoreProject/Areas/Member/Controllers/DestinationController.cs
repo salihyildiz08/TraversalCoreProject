54dd9fe2e906e43910c850a5859eaf0db851941a
Salih YILDIZ <yldzsalih27@gmail.com>
2023-08-08 11:05:33 +0300

    Member area için layout oluşturma ve parçalama

---
+++ TraversalCoreProject/Areas/Member/Controllers/DestinationController.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Areas.Member.Controllers
+{
+    [Area("Member")]
+    [AllowAnonymous]
+    public class DestinationController : Controller
+    {
+        private readonly DestinationManager _destinationManager=new DestinationManager(new EfDestinationDal(new Context()));
+
+
+        public IActionResult Index()
+        {
+            var values = _destinationManager.TGetList();
+
+            return View(values);
+        }
+    }
+}
--- TraversalCoreProject/Startup.cs
+++ TraversalCoreProject/Startup.cs
@@ -72,6 +72,16 @@ namespace TraversalCoreProject
 					name: "default",
 					pattern: "{controller=Home}/{action=Index}/{id?}");
 			});
-		}
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+            });
+
+
+        }
 	}
 }

dd61f29d8b8ea15281a70dac8a13546233e966e3
Salih YILDIZ <yldzsalih27@gmail.com>
2023-08-09 09:44:55 +0300

    Reservation için entity oluşturuldu. Database eklenildi.

---
+++ TraversalCoreProject/Areas/Member/Controllers/ReservationController.cs
@@ -0,0 +1,33 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Areas.Member.Controllers
+{
+    [Area("Member")]
+    public class ReservationController : Controller
+    {
+        [HttpGet]
+        public IActionResult MyCurrentReservation()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult MyOldReservation()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult NewReservation()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult NewReservation(Reservation p)
+        {
+            return View();
+        }
+    }
+}
